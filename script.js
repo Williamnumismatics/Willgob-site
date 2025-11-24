6f4f595327d4bb0dec9f2b40f402dda26852c198
Williamnumismatics <williamnumismatics@gmail.com>
2025-11-25 09:57:52 +1100

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ document.addEventListener('DOMContentLoaded', function() {
     function handleYesClick() {
         resultTextElement.textContent = "Elena is sooo annoying";
         resultTextElement.style.color = "#28a745"; // Green color
+        console.log("yas")
     }
 
     // 4. Define the function for the 'No' button
     function handleNoClick() {
         resultTextElement.textContent = "You declined NO. We will consider your choice.";
         resultTextElement.style.color = "#dc3545"; // Red color
+        console.lop("noo")
     }
 
     // 5. Attach the functions to the button click events (Event Listeners)
